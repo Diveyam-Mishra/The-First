06023c15b0e311750bdc27e15d346845c8ffa5cc
Diveyam-Mishra <mishradiveyam@gmail.com>
2024-10-25 00:51:16 +0530

    8080

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import './Connection/database.js';
 
 dotenv.config();
 const app =express()
+const port=process.env.PORT || 8080
 console.log(process.env)
 app.get('/', (req,res)=> {
     res.send('Diveyam')
+})
+app.listen(port,()=>{
+    console.log('hello')
 })
